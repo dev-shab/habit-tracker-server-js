1fa761383d78de5d515bd821816ff401fa4a8ec7
dev-shab <shabreeshnair1998@gmail.com>
2025-10-05 12:40:59 +0530

    HABIT-1: Setup a basic express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Hello from Habit Tracker");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
+
 console.log("Welcome to habit tracker");
