71bec66d31a8abdc5427361a9dc1c865b7d01e2b
dev-shab <shabreeshnair1998@gmail.com>
2025-10-08 18:39:44 +0530

    HABIT-3: Defined the Habit Schema

---
+++ src/models/Habit.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+const habitSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+    },
+    order: {
+      type: Number,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Habit = model("Habit", habitSchema);
